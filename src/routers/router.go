2af5b580db938639b1711b1fc775e0ea43e32fb1
Irsad99 <irsadmoh01@gmail.com>
2022-07-12 21:48:00 +0700

    Go Deploy

--- src/routers/router.go
+++ src/routers/router.go
@@ -24,8 +24,8 @@ func New() (http.Handler, error) {
 	}
 
 	c := cors.New(cors.Options{
-		AllowedHeaders:   []string{"*"},
-		AllowedMethods:   []string{"*"},
+		AllowedHeaders:   []string{"X-Requested-With", "Content-Type", "Authorization"},
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE"},
 		AllowedOrigins:   []string{"*"},
 		AllowCredentials: true,
 		Debug:            true,
